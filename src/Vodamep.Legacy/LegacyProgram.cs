09f8a218960d61e2c631d3c0f7ca761bad20f0f3
dgorbach <daniel.gorbach@gmail.com>
2018-05-28 20:28:40 +0200

    ReadMdb kann ganzes Jahr

--- src/Vodamep.Legacy/LegacyProgram.cs
+++ src/Vodamep.Legacy/LegacyProgram.cs
@@ -1,5 +1,6 @@
 ﻿using PowerArgs;
 using System;
+using System.Linq;
 using Vodamep.Legacy.Reader;
 
 namespace Vodamep.Legacy
@@ -12,17 +13,37 @@ namespace Vodamep.Legacy
         [ArgDescription("Liest Daten aus einer vgkvdat.mdb.")]
         public void ReadMdb(ReadMdbArgs args)
         {
-            
             var reader = new MdbReader(args.File);
 
             if (args.Year == 0) args.Year = DateTime.Today.AddMonths(-1).Year;
-            if (args.Month == 0) args.Month = DateTime.Today.AddMonths(-1).Month;
 
-            var data = reader.Read(args.Year, args.Month);
 
-            var filename = new Writer().Write(data, args.Json);
+            int[] months;
 
-            Console.WriteLine($"{filename} wurde erzeugt.");
+            if (args.Month == 0)
+            {
+                if (args.Year == DateTime.Today.Year)
+                {
+                    months = Enumerable.Range(1, DateTime.Today.AddMonths(-1).Month).ToArray();
+                }
+                else
+                {
+                    months = Enumerable.Range(1, 12).ToArray();
+                }
+            }
+            else
+            {
+                months = new[] { args.Month };
+            }
+
+            foreach (var month in months)
+            {
+                var data = reader.Read(args.Year, args.Month);
+
+                var filename = new Writer().Write(data, args.Json);
+
+                Console.WriteLine($"{filename} wurde erzeugt.");
+            }
         }
     }
 }
