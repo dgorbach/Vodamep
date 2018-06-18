fc7f0f27eaa53e4389408c19939e63328fba11b6
dgorbach <daniel.gorbach@gmail.com>
2018-06-18 11:00:47 +0200

    dmc validate mit wildcard

--- src/Vodamep.Client/ValidateArgs.cs
+++ src/Vodamep.Client/ValidateArgs.cs
@@ -4,7 +4,7 @@ namespace Vodamep.Client
 {
     public class ValidateArgs
     {        
-        [ArgRequired, ArgExistingFile, ArgPosition(1)]
+        [ArgRequired, ArgPosition(1)]
         public string File { get; set; }
 
         public bool IgnoreWarnings { get; set; } = false;
